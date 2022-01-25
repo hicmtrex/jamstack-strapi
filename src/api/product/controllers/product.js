e4e7595db12d8703b4c1a40e0d5c694bf3716925
Firstname Lastname <your_email@youremail.com>
2022-01-25 22:16:28 +0100

    new

--- src/api/product/controllers/product.js
+++ src/api/product/controllers/product.js
@@ -1,9 +1,9 @@
-'use strict';
+"use strict";
 
 /**
  *  product controller
  */
 
-const { createCoreController } = require('@strapi/strapi').factories;
+const { createCoreController } = require("@strapi/strapi").factories;
 
-module.exports = createCoreController('api::product.product');
+module.exports = createCoreController("api::product.product");
